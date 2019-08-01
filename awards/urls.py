b897c80ec22ad0f533a8d4dad8f806c5dd79c540
Francis Mukuha <mukuha58@gmail.com>
2019-08-01 12:33:21 +0300

    add tinymice url

--- awards/urls.py
+++ awards/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('award.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
+ 
 
 ]
