b581a92dccacc773ecf5fd37e8429aff49f4f67a
Francis Mukuha <mukuha58@gmail.com>
2019-08-01 15:30:24 +0300

    add new project view

--- award/views.py
+++ award/views.py
@@ -31,3 +31,30 @@ def index(request):
         form = SubscribeForm()
 
     return render(request, 'index.html', {'title': title, 'images': images, 'letterForm': form})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.poster = current_user
+            post.save()
+        return redirect('index')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
+
+def search_projects(request):
+    if 'image' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_images(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any person"
+        return render(request, 'search.html', {"message": message})
