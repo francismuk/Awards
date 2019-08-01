e2cedac9ec74b42e583c7bb579ab2c07ecf998c9
Francis Mukuha <mukuha58@gmail.com>
2019-08-01 12:35:39 +0300

    add new project url

--- award/urls.py
+++ award/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name='index'),
+    url(r'^new/project$', views.new_project, name='new-project')
+
     ]
 # if settings.DEBUG:
 #     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
