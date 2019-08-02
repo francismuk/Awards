d88840d13743d85d9aba0ff252f22f7f4c5107b6
Francis Mukuha <mukuha58@gmail.com>
2019-08-02 16:16:15 +0300

    import static filkes

--- award/urls.py
+++ award/urls.py
@@ -8,6 +8,6 @@ urlpatterns=[
     url(r'^new/project$', views.new_project, name='new-project'),
     url(r'^search/', views.search_projects, name='search'),
     url(r'^projects/(\d+)',views.single_post,name ='image'),
-    ]
+]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
